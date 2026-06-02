df594748b6331c9c8e4c1b0b62949ddac9e059f0
masterwee <masterwee@gmail.com>
2026-06-02 23:14:11 +0800

    chore: remove temp debug file

--- check_email.py
+++
@@ -1,9 +0,0 @@
-import django; django.setup()
-from apps.core.models import EmailSetting
-s = EmailSetting.load()
-print("PK:", s.pk, "Host:", repr(s.smtp_host))
-s.smtp_host = "test.example.com"
-s.save()
-s2 = EmailSetting.load()
-print("Reloaded Host:", repr(s2.smtp_host))
-print("Same PK:", s.pk == s2.pk)
